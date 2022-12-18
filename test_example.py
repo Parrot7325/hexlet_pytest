d936367eacfe444b75a36ab75e0b31d91f21bbe4
Parrot7325 <maximus1997@yahoo.com>
2022-12-19 00:04:29 +0300

    Все работает

---
+++ test_example.py
@@ -0,0 +1,11 @@
+import pytest
+from example import reverse
+
+
+def test_reverse():
+    assert reverse('Hexlet') == 'telxeH'
+
+
+def test_reverse_for_empty_string():
+    assert reverse('') == ''
+
