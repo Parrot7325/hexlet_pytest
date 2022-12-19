373fefb8d267e16a22f85bdd68c1261466454237
Parrot7325 <82766453+Parrot7325@users.noreply.github.com>
2022-12-19 18:12:24 +0300

    Update test_example.py

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,11 +1,15 @@
-import pytest
 from hexlet_pytest.example import reverse
 
 
+"""Tests function for reversing string"""
+
+
 def test_reverse():
+    """Usual check"""
     assert reverse('Hexlet') == 'telxeH'
 
 
 def test_reverse_for_empty_string():
+    """Check for empty string"""
     assert reverse('') == ''
 
